3eb953a513bbd6da027225341e2f7ff1aa33288b
Kelly huan <kelly@Kellys-Mac-mini.local>
2026-05-28 17:36:22 +1000

    Update searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,10 @@
-function SearchBar() {
-    return <h1>Search for a book name</h1>;
+function SearchBar({ onSearch }) {
+    return (
+        <div>
+            <h1>Search for a book name</h1>
+            <button>Search</button>
+        </div>
+    )
 }
 
 export default SearchBar;
